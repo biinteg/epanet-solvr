4cbe6992d4c755beb541569ad17444d45cde6e19
biinteg <bintangtpr@gmail.com>
2026-05-10 04:27:20 +0700

    fix: added missing wntr import in auto_solver.py

--- modules/auto_solver.py
+++ modules/auto_solver.py
@@ -5,6 +5,7 @@ from epyt import epanet
 # pyrefly: ignore [missing-import]
 import pandas as pd
 import os
+import wntr
 from modules.helpers import (
     MAX_HEADLOSS_M_PER_KM,
     MAX_VELOCITY_MS,
